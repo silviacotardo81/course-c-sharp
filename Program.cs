7a31e5785ddae5917d631401dc58b21ecb4ab66f
silviacotardo <144198221+chivacotardo81@users.noreply.github.com>
2026-01-22 17:17:53 -0600

    avances del curso de c sharp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var _bob = "Bob";
+var _entero = 3;
+var _flotante = 34.4;
+
+
+
+//decimal x = 12.3m;
+
+
+Console.Write("Hello, " + _bob + "! You have " + _entero + " messages in your inbox. The temperature is " + _flotante + "celsius.");
+
+//Console.Write(34.40M);
