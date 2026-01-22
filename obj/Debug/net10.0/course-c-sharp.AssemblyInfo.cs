9b463d871c7758c8c5ca29ec0df1b66e8abbb61f
silviacotardo <144198221+chivacotardo81@users.noreply.github.com>
2026-01-22 13:24:04 -0600

    chore: add base project structure

--- obj/Debug/net10.0/course-c-sharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/course-c-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("course-c-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0c4fd4638369ab09b47465cfd3debe43bd977d")]
 [assembly: System.Reflection.AssemblyProductAttribute("course-c-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("course-c-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
